6704054931ed9925bbb065cf3207788bdb4efcc4
HeeTae Lee <blader137@gmail.com>
2019-11-12 00:26:29 +0900

    Prototype pattern을 적용하여 문제 해결.

--- PolymorphismTest/Person.cpp
+++ PolymorphismTest/Person.cpp
@@ -18,14 +18,37 @@ public:
 		}
 		return *this;
 	}
-
 	virtual ~Person() {}
+
+	// Functions
+
+	virtual Person* clone() = 0;
+
 	virtual void printOut()
 	{
 		cout << "이름 : " << name << "\n"
 			<< "\n";
 	}
 	const string& getName() const { return this->name; }
+};
+
+class PersonSpawner
+{
+private:
+	Person* prototype;
+
+public:
+	PersonSpawner() { prototype = nullptr; }
+	PersonSpawner(Person* _prototype) : prototype(_prototype) {}
+	Person* makePerson()
+	{
+		return prototype->clone();
+	}
+
+	void setPrototype(Person* _proto)
+	{
+		prototype = _proto;
+	}
 };
 
 class Student : public Person
@@ -52,6 +75,13 @@ public:
 		return *this;
 	}
 	virtual ~Student() {}
+
+	//Functions
+	virtual Person* clone()
+	{
+		return new Student(name, schoolName, stdNum, gpa);
+	}
+
 	virtual void printOut()
 	{
 		cout << "이름 : " << name << "\n"
@@ -96,6 +126,13 @@ public:
 		return *this;
 	}
 	virtual ~SalaryMan() {}
+
+	//Functions
+	virtual Person* clone()
+	{
+		return new SalaryMan(name, companyName, position, salary);
+	}
+
 	virtual void printOut()
 	{
 		cout << "이름 : " << name << "\n"
@@ -117,15 +154,17 @@ public:
 };
 
 
+
+
 int main()
 {
 	int num = 5;
 	Person** personList = new Person * [num];
-	personList[0] = new Student("박한지", "성균관대학교", 201038036, 3.72);
-	personList[1] = new Student("이희태", "서울시립대학교", 201083052, 3.90);
-	personList[2] = new SalaryMan("전승훈", "GS retail", "대리", 340);
-	personList[3] = new Student("신건우", "서강대학교", 201076035, 4.21);
-	personList[4] = new SalaryMan("김상현", "NHN", "팀장", 410);
+	personList[0] = new Student("박승호", "성균관대학교", 201038036, 3.72);
+	personList[1] = new Student("이재형", "서울시립대학교", 201083052, 3.90);
+	personList[2] = new SalaryMan("전민섭", "GS retail", "대리", 340);
+	personList[3] = new Student("신도영", "서강대학교", 201076035, 4.21);
+	personList[4] = new SalaryMan("김철수", "NHN", "팀장", 410);
 
 	for (size_t i = 0; i < num; ++i) {
 		personList[i]->printOut();
@@ -134,10 +173,13 @@ int main()
 
 	// 여기서 Shin이 personList[i] 포인터가 실제로 가리키고 있는 객체의 타입(동적 타입)을 받아서 그 타입의 꼴로
 	// 동적할당 하는 방법을 찾아라.
-	Person* Shin = new auto(*personList[3]);
-	Shin->printOut();
 
-	delete Shin;
+	// Solution : Prototype Pattern.
+	Person* Kim = PersonSpawner(personList[4]).makePerson();
+	Kim->printOut();
+	
+	delete Kim;
+
 	for (size_t i = 0; i < num; ++i) {
 		delete personList[i];
 	}
